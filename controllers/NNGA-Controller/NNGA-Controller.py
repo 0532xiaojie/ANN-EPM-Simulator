81ef3e155f5f6560122c5504b6d7034b1801f1b2
Tanya Howden <tanat@hotmail.co.uk>
2017-03-14 15:05:09 +0000

    started doing fitness function

--- controllers/NNGA-Controller/NNGA-Controller.py
+++ controllers/NNGA-Controller/NNGA-Controller.py
@@ -7,11 +7,16 @@ import numpy as np
 
 sensorNum = 8
 
+def calcFitness():
+	print "calculating fitness"
+	return 0
+
 
 def fitness(weights):
 
 	sensors = []
 	sensorValue = []
+	fitness=0
 
 	print "Fitness"
 
@@ -25,23 +30,31 @@ def fitness(weights):
 	dw.enableEncoders(timestep)
 
 	NN.setWeights(weights)
-	
-	#Use manual timing?
-	#Start timer
 
 	#While (true) loop
-	while (dw.step(timestep) < 100):
-		#Check Timer
-			#Break?
-		print dw.step(timestep)
+	while (dw.step(timestep)!=-1):
 		for i in range(sensorNum):
 			sensorValue[i] = sensors[i].getValue()
+		
+		#Check Timer
+		print dw.getTime()
+		if(dw.getTime() > 300.000):
+    			#calculate fitness
+    			fitness = calcFitness()
+    			
+    			#reset the epuck
+    			
+    			break;
+		
 
 		speed = NN.run(np.array(sensorValue))
 
-		dw.setSpeed(speed[0], speed[1])
+		dw.setSpeed(speed[0]*100, speed[1]*100)
+		
+		
 
-	return 0
+	print "Finished That Round"
+	return fitness
 
 def main():
 	GA.Run()
